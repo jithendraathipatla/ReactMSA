c0cb62842fdeea9cc3f33f65cf6f9aacd0857f10
Harish Mathanan <harish.mathanan@hotmail.com>
2019-06-27 15:31:45 +0800

    Update express server to serve index.html

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.get('/', (req, res) => {
+  res.status(200).sendFile('index.html');
+});
+
+app.use('*', (req, res) => {
+  res.status(404).send('No resource found.')
+});
 
 app.listen(PORT, (error) => {
   if (error) {
