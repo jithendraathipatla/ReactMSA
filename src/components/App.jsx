f655c78ce4ec2231086049010b3756256c7a3d57
Harish Mathanan <harish.mathanan@hotmail.com>
2019-06-27 16:22:15 +0800

    Initialise MSAL library Create and implement sign in functionality Create and implement sign out functionality Create and implement get user functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
+import { UserAgentApplication } from 'msal';
+
 import Navbar from './NavBar';
+import Config from '../../config';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    // initialise User Agent
+    this.userAgentApplication = new UserAgentApplication({
+      auth: {
+        clientId: Config.APP_ID,
+        authority: Config.AUTH_URL,
+        redirectUri: Config.REDIRECT_URL
+      },
+      cache: {
+        cacheLocation: 'localStorage',
+        storeAuthStateInCookie: true
+      }
+    });
+
+    let user = this.userAgentApplication.getAccount();
+
+    this.state = {
+      isAuthenticated: (user !== null),
+      isError: false,
+      user: null,
+      message: ''
+    };
+
+    if (user) {
+      this.getUserProfile();
+    }
   }
 
   render() {
     return (
       <div>
-        <Navbar />
+        <Navbar
+          isAuth={this.state.isAuthenticated}
+          user={this.state.user}
+          signIn={this.signIn}
+          signOut={this.signOut}
+        />
 
         <main>
+          {this.state.isError &&
+            <div className="error">
+              <p>{this.state.message}</p>
+            </div>
+          }
           <div>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
@@ -25,6 +64,64 @@ class App extends React.Component {
         </main>
       </div>
     );
+  }
+
+  // Retrieve the authenticated user
+  getUserProfile = async () => {
+    try {
+
+      let accessToken = await this.userAgentApplication.acquireTokenSilent({
+        scopes: Config.API_SCOPES
+      });
+
+      let user = this.userAgentApplication.getAccount();
+
+      if (accessToken) {
+        this.setState({
+          isAuthenticated: true,
+          user
+        });
+      } else {
+        this.setState({
+          isAuthenticated: false,
+          isError: true,
+          message: 'ERROR: Error retrieving user access token.',
+          user: null
+        });
+      }
+
+    } catch (error) {
+      this.setState({
+        isAuthenticated: false,
+        isError: true,
+        message: JSON.stringify(error),
+        user: null
+      });
+    }
+  }
+
+  signIn = async () => {
+    try {
+      await this.userAgentApplication.loginPopup(
+        {
+          scopes: Config.API_SCOPES,
+          prompt: "select_account"
+      });
+
+      await this.getUserProfile();
+
+    } catch (error) {
+      this.setState({
+        isAuthenticated: false,
+        isError: true,
+        message: JSON.stringify(error),
+        user: null
+      });
+    }
+  }
+
+  signOut = () => {
+    this.userAgentApplication.logout();
   }
 };
 
